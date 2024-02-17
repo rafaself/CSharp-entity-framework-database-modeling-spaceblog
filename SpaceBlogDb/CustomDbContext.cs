73a43ef5552079454e5d0fce0a0855eeb69026ed
rafael <rafaondjango@gmail.com>
2024-02-17 17:32:29 -0300

    Adicionar arquivos de projeto.

---
+++ SpaceBlogDb/CustomDbContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace SpaceBlogDb;
+
+public class CustomDbContext : DbContext
+{
+
+    public CustomDbContext ()
+    {
+
+    }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder options)
+    {
+        string connectionString = "Server=localhost;Database=master;Trusted_Connection=True;";
+        options.UseSqlServer(connectionString);
+    }
+
+}
