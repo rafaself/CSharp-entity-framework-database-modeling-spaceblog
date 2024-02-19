aaa3c53a837d519337deec65cda0efd2212e5625
rafael <rafaondjango@gmail.com>
2024-02-19 16:32:49 -0300

    feat: buscando categorias de posts pelo ID

--- SpaceBlogDb/UseCases/PostCategory/CallPostCategoryUseCases.cs
+++ SpaceBlogDb/UseCases/PostCategory/CallPostCategoryUseCases.cs
@@ -7,17 +7,22 @@ public class CallPostCategoryUseCases
         int option;
         do
         {
-            Console.WriteLine("1 - Criar categoria de post");
+            Console.WriteLine("-- Gerenciamento das Categorias de Posts --\n");
+            Console.WriteLine("1 - Criar Categoria de Post");
+            Console.WriteLine("2 - Buscar Categoria de Post pelo ID");
             Console.WriteLine("0 - Voltar ao menu anterior");
             Console.Write("\nDigite uma opção: ");
 
             var optionTemp = Console.ReadLine() ?? throw new ArgumentNullException("Can't be null!");
             option = int.Parse(optionTemp);
 
-            switch(option)
+            switch (option)
             {
                 case 1:
                     await CreatePostCategory.Execute();
+                    break;
+                case 2:
+                    await GetPostCategoryById.Execute();
                     break;
                 case 0:
                     Console.Clear();
---
+++ SpaceBlogDb/UseCases/PostCategory/GetPostCategoryById.cs
@@ -0,0 +1,22 @@
+﻿using SpaceBlogDb.Repositories;
+
+namespace SpaceBlogDb.UseCases.PostCategory;
+
+public class GetPostCategoryById
+{
+    public static async Task Execute()
+    {
+        var dbContext = new CustomDbContext();
+
+        var postCategoryRepository = new PostCategoryRepository(dbContext);
+
+        Console.Write("Digite o ID da categoria: ");
+        string idTemp = Console.ReadLine() ?? throw new ArgumentNullException("Can't be null!");
+        int id = int.Parse(idTemp);
+
+        var category = await postCategoryRepository.GetByIdAsync(id);
+
+        Console.WriteLine($"#{id} - Categoria: {category.Title}\n");
+
+    }
+}
