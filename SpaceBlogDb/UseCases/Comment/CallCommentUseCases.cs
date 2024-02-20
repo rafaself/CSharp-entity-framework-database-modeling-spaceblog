8560f2a4bbff957bfa91e12397c3f547b4e2d925
rafael <rafaondjango@gmail.com>
2024-02-20 00:15:45 -0300

    feat: buscar comentário pelo id

--- SpaceBlogDb/UseCases/Comment/CallCommentUseCases.cs
+++ SpaceBlogDb/UseCases/Comment/CallCommentUseCases.cs
@@ -9,15 +9,19 @@ public class CallCommentUseCases
         {
             Console.WriteLine("-- Gerenciamento dos Comentários --");
             Console.WriteLine("1 - Criar comentário");
+            Console.WriteLine("2 - Buscar comentário pelo ID");
             Console.WriteLine("0 - Voltar ao menu principal");
             Console.Write("Digite uma opção: ");
             string? optionTemp = Console.ReadLine();
             option = int.Parse(optionTemp!);
 
-            switch(option)
+            switch (option)
             {
                 case 1:
                     await CreateComment.Execute();
+                    break;
+                case 2:
+                    await GetCommentById.Execute();
                     break;
                 case 0:
                     Console.Clear();
---
+++ SpaceBlogDb/UseCases/Comment/GetCommentById.cs
@@ -0,0 +1,26 @@
+﻿using SpaceBlogDb.Repositories;
+
+namespace SpaceBlogDb.UseCases.Comment;
+
+public class GetCommentById
+{
+    public static async Task Execute()
+    {
+        var dbContext = new CustomDbContext();
+
+        var commentRepository = new CommentRepository(dbContext);
+
+        Console.Write("Digite o ID do comentário: ");
+        string? commentIdTemp = Console.ReadLine();
+        int commentId = int.Parse(commentIdTemp);
+
+        var comment = await commentRepository.GetByIdAsync(commentId);
+
+        Console.WriteLine($"\n-- Comentário #{commentId}");
+        Console.WriteLine($"Texto: {comment.Text}");
+        Console.WriteLine($"ID Post: {comment.PostId}");
+        Console.WriteLine($"ID Usuário: {comment.UserId}\n");
+
+
+    }
+}
